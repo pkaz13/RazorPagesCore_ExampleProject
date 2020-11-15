0b39577a950f90fa89fcf8bba1098103b2cd008a
Piotr Kazmierczak <pkazmierczak13@gmail.com>
2020-11-15 11:22:56 +0100

    added authorization for TodoItem folder

--- RazorPagesCore/Startup.cs
+++ RazorPagesCore/Startup.cs
@@ -31,7 +31,10 @@ namespace RazorPagesCore
             {
                 options.UseSqlServer(Configuration.GetConnectionString("TodoListConnection"));
             });
-            services.AddRazorPages();
+            services.AddRazorPages(options =>
+            {
+                options.Conventions.AuthorizeFolder("/TodoItem");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
